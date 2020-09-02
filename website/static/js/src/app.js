5f0d5fea137800c9cb674a0ea330a71cc2fb4934
Seth Epstein <sepstein@sepstein-mbp.lan>
2020-09-02 00:52:16 -0500

    stripping down further

--- website/static/js/src/app.js
+++ website/static/js/src/app.js
@@ -1,58 +1,14 @@
 // NPM imports
 import "fetch-polyfill"
 import "babel-polyfill"
-import "object-fit-videos"
 
 // Local styles
 import styles from "../../css/src/app.scss"
 
-// Local JS
-import Menu from "./components/menu"
-import MenuIcon from "./components/menu_icon"
-import Violator from "./components/violator"
-import IntroVideos from "./components/intro_videos"
-import ScrollIndicator from "./components/ScrollIndicator.js"
-import { muteVideosOnClose, removeInlineStyleIE } from "./components/videos"
 
-import ScrollAnimations from "./helpers/ScrollAnimations.js"
-import LazyLoad from "./helpers/LazyLoad"
-import AspectRatio from "./helpers/AspectRatio"
-import { resizeVideosInModals } from "./helpers/helpers"
-
-// Wagtail Foundation styles
-import styles_vimeo from "../../../../src/wagtail-vimeo/vimeo/static/css/src/vimeo.scss"
-import styles_contact from "../../../../src/wagtail-contact/contact/static/css/src/contact.scss"
-import styles_iframe from "../../../../src/wagtail-iframe/iframe/static/css/src/iframe.scss"
-
-// Wagtail Foundation JS
-import TabFilterJS from "../../../../src/wagtail-tabfilter/tabfilter/static/js/src/tabfilter.js"
-import VimeoJS from "../../../../src/wagtail-vimeo/vimeo/static/js/vimeo.js"
-import ExpandingVideoJS from "../../../../src/wagtail-vimeo/vimeo/static/js/expanding_video.js"
 
 // Initialize components on document ready
 $(document).ready(function() {
-	// FORMAT Javascript_Class.init();
-	AspectRatio.init()
-
-	Menu.init()
-	MenuIcon.init()
-
-	Violator.init()
-
-	TabFilterJS.init()
-
-	VimeoJS.init()
-	resizeVideosInModals()
-
-	ExpandingVideoJS.init()
-	ScrollAnimations.init()
-	IntroVideos.init()
-	LazyLoad.init()
-	ScrollIndicator.init()
-
-	muteVideosOnClose()
-	removeInlineStyleIE()
-
 	// Throttle the resize method
 	let resizeId = null
 	$(window).resize(() => {
@@ -63,5 +19,5 @@ $(document).ready(function() {
 
 // Resize initializers
 let initResizeComponents = function() {
-	AspectRatio.init()
+	console.log('resize')
 }
